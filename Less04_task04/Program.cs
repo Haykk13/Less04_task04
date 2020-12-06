e4bff7b1adbc06d5df4f52ea45f0112dd054ae20
Hayk <hayk.hayr3@gmail.com>
2020-12-06 23:41:50 +0400

    Add project files.

---
+++ Less04_task04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Less04_task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+
+            ((IInter1)instance).Meth1();
+            ((IInter2)instance).Meth1();
+            ((IInter2)instance).Meth2();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IInter1
+    {
+        void Meth1();
+    }
+
+    interface IInter2 : IInter1
+    {
+        void Meth2();
+    }
+
+    class MyClass : IInter2
+    {
+        public void Meth1()
+        {
+            Console.WriteLine("method 1");
+        }
+        public void Meth2()
+        {
+            Console.WriteLine("method 2");
+        }
+    }
+}
